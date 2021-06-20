30c4a1d5c9241e1d0837dc2ef0f3fe2740f03be7
claud-car <claudiocarchietti@yahoo.it>
2021-06-20 17:07:04 +0200

    aggiunte di inizio layout a welcome

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,19 +1,19 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-24 flex items-center">
+        <div class="flex justify-between">
             <div class="flex">
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('home') }}">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex ">
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                        {{ __('Home') }}
                     </x-nav-link>
                     <x-nav-link >
                         {{ __('Restaurants') }}
@@ -21,8 +21,21 @@
                     <x-nav-link >
                         {{ __('About Us') }}
                     </x-nav-link>
+                    <x-nav-link >
+                    @if (Route::has('login'))
+                        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                            @auth
+                            @else
+                                <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
 
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                                @endif
+                            @endauth
+                        </div>
+                    @endif
 
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -52,6 +65,10 @@
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
+                            <x-dropdown-link href="{{ url('/dashboard') }}">
+                                {{ __('Dashboard') }}
+                            </x-dropdown-link>
+
                         </form>
                     </x-slot>
                 </x-dropdown>
@@ -102,6 +119,7 @@
             </div>
         </div>
     </div>
+
 </nav>
 
 
