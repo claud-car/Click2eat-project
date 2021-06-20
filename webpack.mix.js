eae1210db77b9949e410f965045541b5a09cc867
claud-car <claudiocarchietti@yahoo.it>
2021-06-20 10:56:35 +0200

    andre

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,4 +15,9 @@ mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'pub
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
-]);
+])
+.sass('resources/sass/app.scss', 'public/css').options({
+    processCssUrls: false
+});
+
+
