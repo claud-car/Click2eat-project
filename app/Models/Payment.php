e88c824efb82cab07bd2cc8c0f108d442a4c4c9d
andrea-emmanuele <contact@andreaemmanuele.it>
2021-06-21 17:57:05 +0200

    Adding relationship between payments and orders

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'status',
+        'payment_information',
+        'amount',
+    ];
+
+    public function orders()
+    {
+        return $this->hasOne(Order:class);
+    }
 }
