c4097e451a7030a20e0ca91ef0652ea95c38f8b3
claud-car <claudiocarchietti@yahoo.it>
2021-06-25 20:19:17 +0200

    edit and crate plate added

--- resources/views/dashboard/restaurants/plates/create.blade.php
+++ resources/views/dashboard/restaurants/plates/create.blade.php
@@ -1,42 +1,51 @@
-     
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+<x-app-layout>
+<x-slot name="header">
+</x-slot>
+ 
+<h1 class="text-blue text-center text-5xl py-16">Create Plate</h1>
 
-        <form method="POST" action="{{ route('plate.store', ['restaurant' => $restaurant->slug]) }}" enctype="multipart/form-data">
-            @csrf
+<div class="border text-blue container m-auto flex flex-wrap md:justify-evenly w-2/6 h-96 py-8 border rounded-2xl">
+    
+    <!-- Validation Errors -->
+    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+    
+    <form method="POST" action="{{ route('plate.store', ['restaurant' => $restaurant->slug]) }}" enctype="multipart/form-data">
+        @csrf
+    
+        <!-- Name -->
+        <div>
+            <x-label for="name" :value="__('Name')" />
+    
+            <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+        </div>
+    
+        <!-- Description -->
+        <div class="mt-4">
+            <x-label for="description" :value="__('Description')" />
+    
+            <x-input id="description" class="block mt-1 w-full" type="text" name="description" :value="old('description')" required autofocus />
+        </div>
+    
+        <!-- Price -->
+        <div class="mt-4">
+            <x-label for="price" :value="__('Price')" />
+    
+            <x-input id="price" class="block mt-1 w-full" type="text" name="price" :value="old('price')" required autofocus />
+        </div>
 
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
+        <!-- Image -->
+        <div class="form-group py-4">
+            <label for="thumb_path" class="pr-4">Image</label>
+            <input class="form-control-file @error('thumb_path') is-invalid @enderror" id="thumb_path" type="file" name="thumb_path">
+            @error ('thumb_path')
+                <small class=" text-danger"> {{ $message }} </small>
+            @enderror
+        </div>
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
+        <x-button class="bg-blue">
+            {{ __('Apply') }}
+        </x-button>
+    </form>
+</div>
 
-            <!-- Description -->
-            <div class="mt-4">
-                <x-label for="description" :value="__('Description')" />
-
-                <x-input id="description" class="block mt-1 w-full" type="text" name="description" :value="old('description')" required autofocus />
-            </div>
-
-            <!-- Price -->
-            <div class="mt-4">
-                <x-label for="price" :value="__('Price')" />
-
-                <x-input id="price" class="block mt-1 w-full" type="text" name="price" :value="old('price')" required autofocus />
-            </div>
-
-            <div class="form-group">
-                <label for="thumb_path">Image</label>
-                <input class="form-control-file @error('thumb_path') is-invalid @enderror" id="thumb_path" type="file" name="thumb_path">
-                @error ('thumb_path')
-                    <small class=" text-danger"> {{ $message }} </small>
-                @enderror
-            </div>
-
-
-
-            <x-button class="bg-blue ml-4">
-                {{ __('Apply') }}
-            </x-button>
-        </form>
+</x-app-layout>
