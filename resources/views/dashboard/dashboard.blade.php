f3193bc13caaf63c5dae8504cfdd1de4b11133af
flavio-secondini <flavio.secondini@gmail.com>
2021-06-25 12:08:06 +0200

    messaggio di benvenuto dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-white leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
@@ -8,8 +8,8 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                <div class="p-6 bg-blue border-b border-gray-200">
+                    <h1 class="text-white">Benvenuto nella tua pagina personale!</h1>
                 </div>
             </div>
         </div>
