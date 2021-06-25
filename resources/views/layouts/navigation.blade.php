222e3afeb445bf873e3374cd2bf2cf5ce2ad23c9
claud-car <claudiocarchietti@yahoo.it>
2021-06-25 20:39:52 +0200

    added logout route on navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -24,6 +24,14 @@
                     <x-dropdown-link href="{{ url('/dashboard') }}" :active="request()->routeIs('dashboard')">
                                 {{ __('Dashboard') }}
                     </x-dropdown-link>
+                    <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                        <x-dropdown-link :href="route('logout')"
+                                onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-dropdown-link>
+                    </form>
                     <x-nav-link >
                     @if (Route::has('login'))
                         <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
