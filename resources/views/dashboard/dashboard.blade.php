b71dd318a7e39d2894fc4bf0ad7d30f588c216f5
andrea-emmanuele <contact@andreaemmanuele.it>
2021-06-28 16:02:53 +0200

    Added a lot of things

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,5 +1,6 @@
 <x-dashboard-layout>
     <dashboard>
+        <h2 class="text-2xl">Orders</h2>
         <orders-pagination name="id #" subcol="customer address" :items="{{ json_encode($orders) }}" />
     <dashboard/>
 </x-dashboard-layout>
