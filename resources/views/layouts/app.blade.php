71c6815ad104a462a0dfbfeb41ef7910812351b7
claud-car <claudiocarchietti@yahoo.it>
2021-06-28 17:00:27 +0200

    color white in welcome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
     </head>
 
     <body class="font-sans antialiased">
-        <div id="app" class="min-h-screen bg-gray-100">
+        <div id="app" class="min-h-screen">
             @include('layouts.navigation')
 
             <!-- Header -->
