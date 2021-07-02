bc7ff9962463f5d553ff270c36065c4b0d532958
flavio-secondini <flavio.secondini@gmail.com>
2021-07-02 08:48:38 +0200

    merged right changes

--- resources/views/cart/show.blade.php
+++ resources/views/cart/show.blade.php
@@ -21,51 +21,7 @@
         </div>
     </div>
 
-    {{-- checkout --}}
-    <div class="">
-        {{-- checkout table --}}
-        <div class="my-20 flex flex-col justify-center">
-            <div class="mx-auto bg-gray-100 w-11/12 md:w-96 rounded-lg transform lg:translate-x-60">
-                <div class="box-content p-10">
-                    <div class="py-2 flex flex-row justify-between border-b-2 border-gray-200 font-semibold text-gray-600">
-                        <div>
-                            <p>Subtotal</p>
-                        </div>
-                        <div>
-                            <p>Costo$</p>
-                        </div>
-                    </div>
-                    <div class="py-2 flex flex-row justify-between border-b-2 border-gray-200 font-semibold text-gray-600">
-                        <div>
-                            <p>Delivery</p>
-                        </div>
-                        <div>
-                            <p>25$</p>
-                        </div>
-                    </div>
-                    <div class="py-2 flex flex-row justify-between border-b-2 border-gray-200 font-semibold text-gray-600">
-                        <div>
-                            <p>Total</p>
-                        </div>
-                        <div>
-                            <p>Costo$</p>
-                        </div>
-                    </div>
-                </div> 
-                 {{-- button --}}
-                <div class="mb-10 mx-5 flex justify-center items-center">
-                    <button class="w-full bg-orange bg-gradient-to-r from-yellow to-orange rounded-full">
-                        <h3 class="text-white text-xl p-5"><a href="cart/checkout">Checkout</a></h3>
-                    </button>
-                </div>             
-            </div>           
-        </div>
-    </div>
-
-    <x-slot name="content">
-    </x-slot>
-    
-</x-app-layout>
+    <checkout-table />
 
     <x-slot name="content"></x-slot>
 </x-app-layout>
