4735c3051bc76591dc69f75f93eac3cdc3177471
flavio-secondini <flavio.secondini@gmail.com>
2021-07-04 12:27:39 +0200

    text xxs added to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            fontSize: {
+                'xxs': '.6rem',
+               },
             colors: {
                 transparent: 'transparent',
                 white: {
