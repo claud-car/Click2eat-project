340ab65bf385a619fff65e6fdae6b8f52ec0590a
andrea-emmanuele <contact@andreaemmanuele.it>
2021-07-05 15:16:11 +0200

    added last orders chart

---
+++ resources/js/components/dashboard/Stats.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="w-2/3 bg-white rounded-md border-2 border-gray-200 mb-12">
+        <canvas ref="lastorders" class="my-4 mx-6"></canvas>
+    </div>
+</template>
+
+<script>
+import { Chart, ChartConfiguration, LineController, LineElement, PointElement, LinearScale, Title, CategoryScale } from 'chart.js'
+import ordersData from "../../stats-data";
+
+const axios = require('axios')
+
+export default {
+    name: 'Stats',
+    props: ['id'],
+    data() {
+        return {
+            ordersData,
+            orders: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        }
+    },
+    mounted() {
+        Chart.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale);
+
+        this.getLastOrders();
+    },
+    methods: {
+        getLastOrders() {
+            axios.get('/api/last-orders', {
+                params: {
+                    id: this.id
+                }
+            })
+            .then(response => {
+                let months = response.data
+
+                let i = 0;
+
+                Object.keys(months).forEach(month => {
+                    if (month[0].includes('0') ) {
+                        month = month[1]
+                    }
+
+                    this.orders[parseInt(month - 1)] = Object.values(months)[i]
+                    i++
+                })
+
+                this.ordersData.data.datasets[0].data = this.orders
+
+                const chart = new Chart(this.$refs.lastorders, this.ordersData);
+            })
+        }
+    }
+}
+</script>
--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,7 +1,10 @@
 <x-dashboard-layout>
     <dashboard>
+        <h2 class="text-2xl mb-4">Last Received Orders</h2>
+        <div>
+            <Stats :id="{{ json_encode(\Illuminate\Support\Facades\Auth::id()) }}"/>
+        </div>
         <h2 class="text-2xl">Orders</h2>
         <orders-pagination name="id #" subcol="customer address" :items="{{ json_encode($orders) }}" />
     <dashboard/>
-    
 </x-dashboard-layout>
