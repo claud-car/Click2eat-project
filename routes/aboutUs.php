4c96bc9b3d189a8f65658c2f987b29d150dc7112
flavio-secondini <flavio.secondini@gmail.com>
2021-07-05 19:31:19 +0200

    commit finale

--- routes/aboutUs.php
+++ routes/aboutUs.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\AboutUsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('aboutUs/', [AboutUsController::class, 'show'])
-                ->name('aboutUs.show');
+                ->name('aboutUs');
