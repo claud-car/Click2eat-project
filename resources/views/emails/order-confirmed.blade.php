ffd42dd49775c57393d0e9c499c82a44c6ed7c9b
claud-car <claudiocarchietti@yahoo.it>
2021-07-05 21:57:52 +0200

    q

--- resources/views/emails/order-confirmed.blade.php
+++ resources/views/emails/order-confirmed.blade.php
@@ -1,5 +1,5 @@
 @component('mail::message')
     # Order Confirmation
     We've got your order!
-    Thanks you for choose us, we cook very well.
+    Thank you for choose us, we cook very well.
 @endcomponent
