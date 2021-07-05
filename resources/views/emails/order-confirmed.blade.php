a95c7012e2811d14d0c2b3f7e6d326a123ba4a46
claud-car <claudiocarchietti@yahoo.it>
2021-07-05 22:53:19 +0200

    fixed some text

--- resources/views/emails/order-confirmed.blade.php
+++ resources/views/emails/order-confirmed.blade.php
@@ -1,5 +1,5 @@
 @component('mail::message')
     # Order Confirmation
     We've got your order!
-    Thank you for choose us, we cook very well.
+    Thank you for choosing us, we cook very well.
 @endcomponent
