02bb5f539b637fbafd71f09213e8c3dfaea23719
andrea-emmanuele <contact@andreaemmanuele.it>
2021-07-06 10:29:08 +0200

    adj

--- resources/views/restaurants/show.blade.php
+++ resources/views/restaurants/show.blade.php
@@ -9,7 +9,7 @@
 
     <div class="text-blue">
         <div class="text-center h-80 w-full px-6 lg:w-2/3 xl:w-1/2 lg:mx-auto flex flex-col justify-center mt-12 lg:mt-32">
-            <div class="w-full h-80 rounded-lg" style="background:url({{ $restaurant->thumb_path }});background-repeat: no-repeat; background-size:cover"></div>
+            <div class="w-full h-80 rounded-lg" style="background:url({{ $restaurant->thumb_path }});background-repeat: no-repeat; background-size:cover; background-repeat: round"></div>
         </div>
         <h2 class="text-5xl text-center mt-16">{{ $restaurant->name }}</h2>
         <h2 class="norican text-center text-4xl my-14 text-yellow">Amazing Taste</h2>
